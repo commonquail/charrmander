87e4281e8e7b20d873723730a99104e99075b2e4
Mikkel Kjeldsen <commonquail@gmail.com>
2012-12-12 21:41:28 +0100

    Move INotifyPropertyChanged to AbstractNotifier

--- Model/Character.cs
+++ Model/Character.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Diagnostics;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
+using Charrmander.Util;
 
 namespace Charrmander.Model
 {
-    class Character : INotifyPropertyChanged
+    class Character : AbstractNotifier
     {
         private string _name;
         private string _profession;
@@ -44,44 +45,6 @@ namespace Charrmander.Model
         public Character()
         {
             Areas = new ObservableCollection<Area>();
-        }
-
-        /// <summary>
-        /// Warns the developer if this object does not have
-        /// a public property with the specified name. This 
-        /// method does not exist in a Release build.
-        /// </summary>
-        [Conditional("DEBUG")]
-        [DebuggerStepThrough]
-        public void VerifyPropertyName(string propertyName)
-        {
-            // Verify that the property name matches a real,  
-            // public, instance property on this object.
-            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
-            {
-                Debug.Fail("Invalid property name: " + propertyName);
-            }
-        }
-
-        /// <summary>
-        /// Raised when a property on this object has a new value.
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            this.VerifyPropertyName(propertyName);
-
-            PropertyChangedEventHandler handler = this.PropertyChanged;
-            if (handler != null)
-            {
-                var e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
         }
     }
 }
--- ViewModel/ViewModelMain.cs
+++ ViewModel/ViewModelMain.cs
@@ -9,10 +9,11 @@ using System.Windows;
 using System.Xml;
 using System.Xml.Linq;
 using Charrmander.Model;
+using Charrmander.Util;
 
 namespace Charrmander.ViewModel
 {
-    class ViewModelMain : INotifyPropertyChanged
+    class ViewModelMain : AbstractNotifier
     {
         private Character _selectedCharacter;
 
@@ -304,44 +305,6 @@ namespace Charrmander.ViewModel
                     Skills = xe.Element("Skills").Value,
                     Vistas = xe.Element("Vistas").Value
                 });
-            }
-        }
-
-        /// <summary>
-        /// Warns the developer if this object does not have
-        /// a public property with the specified name. This 
-        /// method does not exist in a Release build.
-        /// </summary>
-        [Conditional("DEBUG")]
-        [DebuggerStepThrough]
-        public void VerifyPropertyName(string propertyName)
-        {
-            // Verify that the property name matches a real,  
-            // public, instance property on this object.
-            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
-            {
-                Debug.Fail("Invalid property name: " + propertyName);
-            }
-        }
-
-        /// <summary>
-        /// Raised when a property on this object has a new value.
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            this.VerifyPropertyName(propertyName);
-
-            PropertyChangedEventHandler handler = this.PropertyChanged;
-            if (handler != null)
-            {
-                var e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
             }
         }
     }
