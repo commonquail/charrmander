6d6863946db03cdf7a9cebb612ba6deb78eef0ad
Mikkel Kjeldsen <commonquail@gmail.com>
2012-12-18 00:24:42 +0100

    Add view for displaying new update info
    Still needs refinement.

---
+++ View/UpdateAvailableView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Diagnostics;
+
+namespace Charrmander.View
+{
+    /// <summary>
+    /// Interaction logic for UpdateAvailableView.xaml
+    /// </summary>
+    public partial class UpdateAvailableView : Window
+    {
+        public UpdateAvailableView()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
---
+++ ViewModel/UpdateAvailableViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charrmander.Util;
+using System.Windows;
+using Charrmander.View;
+
+namespace Charrmander.ViewModel
+{
+    class UpdateAvailableViewModel : AbstractNotifier
+    {
+        public UpdateAvailableViewModel()
+        {
+            Window window = new UpdateAvailableView();
+            window.DataContext = this;
+            window.Show();
+        }
+    }
+}
