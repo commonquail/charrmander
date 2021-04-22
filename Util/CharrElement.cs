7d760a1a7a05a54401beb6e04bbdc20bb3927a53
Mikkel Kjeldsen <commonquail@gmail.com>
2021-04-22 22:42:37 +0200

    Fix CA2211: Non-constant fields should not be visible

--- Util/CharrElement.cs
+++ Util/CharrElement.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Xml.Linq;
 
 namespace Charrmander.Util
 {
     public class CharrElement : XElement
     {
-        public static XNamespace Charr = Properties.Resources.xNamespace;
+        public static XNamespace Charr { get; set; } = Properties.Resources.xNamespace;
 
         public CharrElement(string elementName, params Object[] content)
             : base(Charr + elementName, content)
