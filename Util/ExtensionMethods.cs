bd10a47fe7726879a391c3b99b326f87417853c6
Mikkel Kjeldsen <commonquail@gmail.com>
2021-04-22 22:38:18 +0200

    Fix CA1825: Avoid zero-length array allocations

--- Util/ExtensionMethods.cs
+++ Util/ExtensionMethods.cs
@@ -55,7 +55,7 @@ namespace Charrmander.Util
             var ce = xe.Elements(CharrElement.Charr + name);
             if (ce == null)
             {
-                ce = new CharrElement[0];
+                ce = Array.Empty<CharrElement>();
             }
             return ce;
         }
@@ -76,7 +76,7 @@ namespace Charrmander.Util
             var cd = xc.Descendants(CharrElement.Charr + name);
             if (cd == null)
             {
-                cd = new CharrElement[0];
+                cd = Array.Empty<CharrElement>();
             }
             return cd;
         }
