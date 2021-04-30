1c6f52bfb77a86e25ea38b973e01f381eb55ad73
Mikkel Kjeldsen <commonquail@gmail.com>
2021-04-30 21:13:57 +0200

    Migrate from MSTest to xUnit.net

--- ExtensionsTestProject/ExtensionMethodsTest.cs
+++ ExtensionsTestProject/ExtensionMethodsTest.cs
@@ -1,73 +1,15 @@
 using Charrmander.Util;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Xunit;
 
 namespace ExtensionsTestProject
 {
-
-
-    /// <summary>
-    ///This is a test class for ExtensionMethodsTest and is intended
-    ///to contain all ExtensionMethodsTest Unit Tests
-    ///</summary>
-    [TestClass()]
     public class ExtensionMethodsTest
     {
-
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
-
         /// <summary>
         /// Any version is newer than <c>null</c>.
         /// </summary>
-        [TestMethod()]
+        [Fact]
         public void AnyIsNewerThanNull()
         {
             Version v = new("0.0.0.0");
@@ -75,13 +17,13 @@ namespace ExtensionsTestProject
             bool expected = true;
             bool actual;
             actual = ExtensionMethods.IsNewerThan(v, other);
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         /// <summary>
         /// 1.0.0.0 is not newer than 1.0.0.0.
         /// </summary>
-        [TestMethod()]
+        [Fact]
         public void NoneIsNewerThanEqual()
         {
             var v = new Version("1.0.0.0");
@@ -89,26 +31,26 @@ namespace ExtensionsTestProject
             bool expected = false;
             bool actual;
             actual = ExtensionMethods.IsNewerThan(v, other);
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         /// <summary>
         /// The same instance is not newer than itself.
         /// </summary>
-        [TestMethod()]
+        [Fact]
         public void InstanceNotNewerThanSelf()
         {
             var v = new Version("1.0.0.0");
             bool expected = false;
             bool actual;
             actual = ExtensionMethods.IsNewerThan(v, v);
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         /// <summary>
         /// 0.0.0.1 is newer than 0.0.0.0.
         ///</summary>
-        [TestMethod()]
+        [Fact]
         public void RevisionOneIsNewerThanRevisionZero()
         {
             var v = new Version("0.0.0.1");
@@ -116,13 +58,13 @@ namespace ExtensionsTestProject
             bool expected = true;
             bool actual;
             actual = ExtensionMethods.IsNewerThan(v, other);
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         /// <summary>
         /// 0.0.0.0 is note newer than 0.0.0.1.
         ///</summary>
-        [TestMethod()]
+        [Fact]
         public void RevisionZeroIsNotNewerThanRevisionOne()
         {
             var v = new Version("0.0.0.0");
@@ -130,13 +72,13 @@ namespace ExtensionsTestProject
             bool expected = false;
             bool actual;
             actual = ExtensionMethods.IsNewerThan(v, other);
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         /// <summary>
         /// 0.0.1.0 is newer than 0.0.0.9.
         ///</summary>
-        [TestMethod()]
+        [Fact]
         public void BuildOneIsNewerThanRevisionNine()
         {
             var v = new Version("0.0.1.0");
@@ -144,13 +86,13 @@ namespace ExtensionsTestProject
             bool expected = true;
             bool actual;
             actual = ExtensionMethods.IsNewerThan(v, other);
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         /// <summary>
         /// 0.0.1.0 is newer than 0.0.0.
         ///</summary>
-        [TestMethod()]
+        [Fact]
         public void BuildOneIsNewerThanNoRevision()
         {
             var v = new Version("0.0.1.0");
@@ -158,13 +100,13 @@ namespace ExtensionsTestProject
             bool expected = true;
             bool actual;
             actual = ExtensionMethods.IsNewerThan(v, other);
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         /// <summary>
         /// 1.0.0.0 is newer than 0.9.
         ///</summary>
-        [TestMethod()]
+        [Fact]
         public void MajorOneIsNewerThanMinorNine()
         {
             var v = new Version("1.0.0.0");
@@ -172,7 +114,7 @@ namespace ExtensionsTestProject
             bool expected = true;
             bool actual;
             actual = ExtensionMethods.IsNewerThan(v, other);
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
     }
 }
