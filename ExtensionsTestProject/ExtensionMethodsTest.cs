28e8f27949a11c01947a581530e305e29d016968
Mikkel Kjeldsen <commonquail@gmail.com>
2021-05-01 17:32:49 +0200

    Replace explict tests with theory
    All current test details generalize into data input for xUnit.net's
    "theory" test With commit 602bda5 (Use FluentAssertions, 2021-04-30) we
    also have access to assertion messages so all tests can be rewritten at
    no loss to comprehension.

--- ExtensionsTestProject/ExtensionMethodsTest.cs
+++ ExtensionsTestProject/ExtensionMethodsTest.cs
@@ -7,123 +7,22 @@ namespace ExtensionsTestProject
 {
     public class ExtensionMethodsTest
     {
-        /// <summary>
-        /// Any version is newer than <c>null</c>.
-        /// </summary>
-        [Fact]
-        public void AnyIsNewerThanNull()
+        [Theory]
+        [InlineData("0.0.0.0", null, true, @"""any is newer than null""")]
+        [InlineData("1.0.0.0", "1.0.0.0", false, @"""self is not newer than self""")]
+        [InlineData("0.0.0.1", "0.0.0.0", true, @"""revision one is newer than revision zero""")]
+        [InlineData("0.0.0.0", "0.0.0.1", false, @"""revision zero is not newer than revision one""")]
+        [InlineData("0.0.1.0", "0.0.0.9", true, @"""build one is newer than revision 9""")]
+        [InlineData("0.0.1.0", "0.0.0", true, @"""build one is newer than no revision""")]
+        [InlineData("1.0.0.0", "0.9", true, @"""major one is newer than minor nine""")]
+        public void lhs_is_newer_than_rhs(string lhs, string rhs, bool expected, string reason)
         {
-            Version v = new("0.0.0.0");
-            Version other = null;
-            bool expected = true;
+            var v = new Version(lhs);
+            var other = rhs == null ? null : new Version(rhs);
             bool actual;
             actual = ExtensionMethods.IsNewerThan(v, other);
 
-            actual.Should().Be(expected);
-        }
-
-        /// <summary>
-        /// 1.0.0.0 is not newer than 1.0.0.0.
-        /// </summary>
-        [Fact]
-        public void NoneIsNewerThanEqual()
-        {
-            var v = new Version("1.0.0.0");
-            var other = new Version("1.0.0.0");
-            bool expected = false;
-            bool actual;
-            actual = ExtensionMethods.IsNewerThan(v, other);
-
-            actual.Should().Be(expected);
-        }
-
-        /// <summary>
-        /// The same instance is not newer than itself.
-        /// </summary>
-        [Fact]
-        public void InstanceNotNewerThanSelf()
-        {
-            var v = new Version("1.0.0.0");
-            bool expected = false;
-            bool actual;
-            actual = ExtensionMethods.IsNewerThan(v, v);
-
-            actual.Should().Be(expected);
-        }
-
-        /// <summary>
-        /// 0.0.0.1 is newer than 0.0.0.0.
-        ///</summary>
-        [Fact]
-        public void RevisionOneIsNewerThanRevisionZero()
-        {
-            var v = new Version("0.0.0.1");
-            var other = new Version("0.0.0.0");
-            bool expected = true;
-            bool actual;
-            actual = ExtensionMethods.IsNewerThan(v, other);
-
-            actual.Should().Be(expected);
-        }
-
-        /// <summary>
-        /// 0.0.0.0 is note newer than 0.0.0.1.
-        ///</summary>
-        [Fact]
-        public void RevisionZeroIsNotNewerThanRevisionOne()
-        {
-            var v = new Version("0.0.0.0");
-            var other = new Version("0.0.0.1");
-            bool expected = false;
-            bool actual;
-            actual = ExtensionMethods.IsNewerThan(v, other);
-
-            actual.Should().Be(expected);
-        }
-
-        /// <summary>
-        /// 0.0.1.0 is newer than 0.0.0.9.
-        ///</summary>
-        [Fact]
-        public void BuildOneIsNewerThanRevisionNine()
-        {
-            var v = new Version("0.0.1.0");
-            var other = new Version("0.0.0.9");
-            bool expected = true;
-            bool actual;
-            actual = ExtensionMethods.IsNewerThan(v, other);
-
-            actual.Should().Be(expected);
-        }
-
-        /// <summary>
-        /// 0.0.1.0 is newer than 0.0.0.
-        ///</summary>
-        [Fact]
-        public void BuildOneIsNewerThanNoRevision()
-        {
-            var v = new Version("0.0.1.0");
-            var other = new Version("0.0.0");
-            bool expected = true;
-            bool actual;
-            actual = ExtensionMethods.IsNewerThan(v, other);
-
-            actual.Should().Be(expected);
-        }
-
-        /// <summary>
-        /// 1.0.0.0 is newer than 0.9.
-        ///</summary>
-        [Fact]
-        public void MajorOneIsNewerThanMinorNine()
-        {
-            var v = new Version("1.0.0.0");
-            var other = new Version("0.9");
-            bool expected = true;
-            bool actual;
-            actual = ExtensionMethods.IsNewerThan(v, other);
-
-            actual.Should().Be(expected);
+            actual.Should().Be(expected, reason);
         }
     }
 }
