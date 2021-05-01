6fbc16d71858ea2b9e446577423f869b8d962391
Mikkel Kjeldsen <commonquail@gmail.com>
2021-05-01 18:46:56 +0200

    Add test case for numerical over lexicographical ordering

--- ExtensionsTestProject/ExtensionMethodsTest.cs
+++ ExtensionsTestProject/ExtensionMethodsTest.cs
@@ -13,6 +13,7 @@ namespace ExtensionsTestProject
         [InlineData("0.0.0.1", "0.0.0.0", true, @"""revision one is newer than revision zero""")]
         [InlineData("0.0.0.0", "0.0.0.1", false, @"""revision zero is not newer than revision one""")]
         [InlineData("0.0.1.0", "0.0.0.9", true, @"""build one is newer than revision 9""")]
+        [InlineData("0.0.0.10", "0.0.0.1", true, @"""revision 10 is newer than revision 1""")]
         [InlineData("0.0.1.0", "0.0.0", true, @"""build one is newer than no revision""")]
         [InlineData("1.0.0.0", "0.9", true, @"""major one is newer than minor nine""")]
         public void lhs_is_newer_than_rhs(string lhs, string rhs, bool expected, string reason)
