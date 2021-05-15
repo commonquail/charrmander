commonquail/charrmander

commit d750c9b946ef2173b3b6c73f77f234fd20103ae0

Move IValueConverter implementations to own files

View/GameIconConverter.cs
@@ -1,0 +1,22 @@
+﻿using Charrmander.Model;
+using System;
+using System.Windows.Data;
+
+namespace Charrmander.View
+{
+    /// <summary>
+    /// Converts the <see cref="Character.Profession"/> property to the corresponding icon.
+    /// </summary>
+    public class GameIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return "/Icons/Game/" + parameter + "/" + value + ".png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}

View/IntFromBlankTextConverter.cs
@@ -1,0 +1,28 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Charrmander.View
+{
+    /// <summary>
+    /// Converts a blank or null string to <c>0</c>.
+    /// </summary>
+    public class IntFromBlankTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var str = value?.ToString();
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return 0;
+            }
+            _ = int.TryParse(str, out int res);
+
+            return res;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.ToString();
+        }
+    }
+}

View/KeyRewardBoolConverter.cs
@@ -1,0 +1,23 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Charrmander.View
+{
+    /// <summary>
+    /// Converts a check box checked state to a key if <c>True</c>, empty string otherwise.
+    /// </summary>
+    public class KeyRewardBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (value is bool hasKeyReward && hasKeyReward)
+                ? "\U0001F5DD"
+                : string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}

View/NamelessCharacterConverter.cs
@@ -1,0 +1,22 @@
+﻿using Charrmander.Model;
+using System;
+using System.Windows.Data;
+
+namespace Charrmander.View
+{
+    /// <summary>
+    /// Converts the <see cref="Character.Name"/> property if it is unset.
+    /// </summary>
+    public class NamelessCharacterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return string.IsNullOrWhiteSpace((value as string)) ? "[Unnamed]" : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}

View/CompletionStateConverter.cs
@@ -1,0 +1,52 @@
+using Charrmander.Model;
+using System;
+using System.Windows.Data;
+
+namespace Charrmander.View
+{
+    /// <summary>
+    /// Converts <see cref="CompletionState"/>s into stars imitating area progression icons.
+    /// </summary>
+    public class CompletionStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string res = string.Empty;
+            CompletionState state = CompletionState.NotBegun;
+            switch (value)
+            {
+                case CompletionState v:
+                    state = v;
+                    break;
+                case string v:
+                    if (Enum.TryParse(v, out CompletionState s))
+                    {
+                        state = s;
+                    }
+
+                    break;
+            }
+
+            switch (state)
+            {
+                case CompletionState.NotBegun:
+                    break;
+                case CompletionState.Begun:
+                    res = "\u2606"; // White star.
+                    break;
+                case CompletionState.Completed:
+                    res = "\u2605"; // Black star.
+                    break;
+                default:
+                    break;
+            }
+
+            return res;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}

View/MainWindow.xaml.cs
@@ -1,9 +1,6 @@
-using Charrmander.Model;
 using Charrmander.ViewModel;
-using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 
 namespace Charrmander.View
 {
@@ -57,125 +54,6 @@ namespace Charrmander.View
         private void CharacterNameTextBox_LostKeyboardFocus(object sender, RoutedEventArgs e)
         {
             (DataContext as ViewModelMain).TrimSelectedCharacterName();
-        }
-    }
-
-    /// <summary>
-    /// Converts the <see cref="Character.Name"/> property if it is unset.
-    /// </summary>
-    public class NamelessCharacterConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return string.IsNullOrWhiteSpace((value as string)) ? "[Unnamed]" : value;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
-    }
-
-    /// <summary>
-    /// Converts the <see cref="Character.Profession"/> property to the corresponding icon.
-    /// </summary>
-    public class GameIconConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return "/Icons/Game/" + parameter + "/" + value + ".png";
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
-    }
-
-    /// <summary>
-    /// Converts <see cref="CompletionState"/>s into stars imitating area progression icons.
-    /// </summary>
-    public class CompletionStateConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            string res = string.Empty;
-            CompletionState state = CompletionState.NotBegun;
-            switch (value)
-            {
-                case CompletionState v:
-                    state = v;
-                    break;
-                case string v:
-                    if (Enum.TryParse(v, out CompletionState s))
-                    {
-                        state = s;
-                    }
-
-                    break;
-            }
-
-            switch (state)
-            {
-                case CompletionState.NotBegun:
-                    break;
-                case CompletionState.Begun:
-                    res = "\u2606"; // White star.
-                    break;
-                case CompletionState.Completed:
-                    res = "\u2605"; // Black star.
-                    break;
-                default:
-                    break;
-            }
-
-            return res;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
-    }
-
-    /// <summary>
-    /// Converts a check box checked state to a key if <c>True</c>, empty string otherwise.
-    /// </summary>
-    public class KeyRewardBoolConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return (value is bool hasKeyReward && hasKeyReward)
-                ? "\U0001F5DD"
-                : string.Empty;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
-    }
-
-    /// <summary>
-    /// Converts a blank or null string to <c>0</c>.
-    /// </summary>
-    public class IntFromBlankTextConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            var str = value?.ToString();
-            if (string.IsNullOrWhiteSpace(str))
-            {
-                return 0;
-            }
-            _ = int.TryParse(str, out int res);
-
-            return res;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return value.ToString();
         }
     }
 }

commit f68dc74bd96614c1a688bb60e014a008ae5bd565

Remove BOMs
Fixes: d750c9b (Move IValueConverter implementations to own files, 2021-05-13)

View/GameIconConverter.cs
@@ -1,4 +1,4 @@
-﻿using Charrmander.Model;
+using Charrmander.Model;
 using System;
 using System.Windows.Data;
 

View/IntFromBlankTextConverter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Data;
 
 namespace Charrmander.View

View/KeyRewardBoolConverter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Data;
 
 namespace Charrmander.View

View/NamelessCharacterConverter.cs
@@ -1,4 +1,4 @@
-﻿using Charrmander.Model;
+using Charrmander.Model;
 using System;
 using System.Windows.Data;
 