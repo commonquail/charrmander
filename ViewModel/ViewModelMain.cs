commonquail/charrmander

commit ffec7a62be2ffd8e77ae4dc3e077998971954e50

Compute locked skill points on "Areas" tab
I've routinely engaged in counting locked skill/hero challenges in HoT
and PoF to see which character to pick for a run. Automating that count
is trivial so there is no need to delegate that to the user.

VersionHistory.xml
@@ -6,6 +6,7 @@
       <DownloadUrl>https://bitbucket.org/jokum/charrmander/downloads/charrmander-1.23.0.zip</DownloadUrl>
       <Notes>
         <Note>Don't crash if update download fails.</Note>
+        <Note>Compute locked hero points per game region on the "Areas" tab.</Note>
       </Notes>
     </Release>
   </Unreleased>

ViewModel/ViewModelMain.cs
@@ -100,10 +100,17 @@ namespace Charrmander.ViewModel
                 {
                     worldCompletionAreaNames.Add(a.Name);
                 }
+                CountAreaSkillPointsInto(SkillPointsTotal, a);
             }
             AreaReferenceList = areas;
             ReferenceAreaNames = areaNames;
             WorldCompletionAreaNames = worldCompletionAreaNames;
+            _skillPointsLocked = new()
+            {
+                Core = SkillPointsTotal.Core,
+                Hot = SkillPointsTotal.Hot,
+                Pof = SkillPointsTotal.Pof,
+            };
 
             static bool IsRequiredForWorldCompletion(Area a) => a.ParticipatesInWorldCompletion;
 
@@ -308,6 +315,7 @@ namespace Charrmander.ViewModel
                             SelectedAreaCharacter = null;
                         }
                         ChangedAreaOrCharacter();
+                        CountLockedSkillPointsOf(_selectedCharacter);
                     }
                     IsCharacterDetailEnabled = value != null;
                     RaisePropertyChanged(nameof(SelectedCharacter));
@@ -583,6 +591,7 @@ namespace Charrmander.ViewModel
                     && !string.IsNullOrWhiteSpace(value))
                 {
                     SelectedAreaCharacter.Skills = value;
+                    CountLockedSkillPointsOf(SelectedCharacter);
                     RaisePropertyChanged(nameof(Skills));
                     RaisePropertyChanged(nameof(SkillIcon));
                     UpdateAreaState(SelectedAreaReference, SelectedCharacter, WorldCompletionAreaNames);
@@ -697,6 +706,68 @@ namespace Charrmander.ViewModel
                     return "VistaDone";
                 }
                 return "Vista";
+            }
+        }
+
+        public SkillPointsByGame SkillPointsTotal { get; } = new();
+
+        private SkillPointsByGame _skillPointsUnlocked = new();
+        public SkillPointsByGame SkillPointsUnlocked
+        {
+            get => _skillPointsUnlocked;
+            set
+            {
+                _skillPointsUnlocked = value;
+                RaisePropertyChanged(nameof(SkillPointsUnlocked));
+            }
+        }
+
+        private SkillPointsByGame _skillPointsLocked;
+        public SkillPointsByGame SkillPointsLocked
+        {
+            get => _skillPointsLocked;
+            set
+            {
+                _skillPointsLocked = value;
+                RaisePropertyChanged(nameof(SkillPointsLocked));
+            }
+        }
+
+        private void CountLockedSkillPointsOf(Character character)
+        {
+            var sp = new SkillPointsByGame();
+            foreach (var a in character.AreaByName.Values)
+            {
+                CountAreaSkillPointsInto(sp, a);
+            }
+
+            SkillPointsUnlocked = sp;
+            SkillPointsLocked.Core = SkillPointsTotal.Core - SkillPointsUnlocked.Core;
+            SkillPointsLocked.Hot = SkillPointsTotal.Hot - SkillPointsUnlocked.Hot;
+            SkillPointsLocked.Pof = SkillPointsTotal.Pof - SkillPointsUnlocked.Pof;
+        }
+
+        private static void CountAreaSkillPointsInto(SkillPointsByGame byGame, Area a)
+        {
+            var skillPoints = int.Parse(a.Skills);
+            switch (a.Name)
+            {
+                case "Auric Basin":
+                case "Dragon's Stand":
+                case "Tangled Depths":
+                case "Verdant Brink":
+                    byGame.Hot += skillPoints * 10;
+                    break;
+                case "Crystal Oasis":
+                case "Desert Highlands":
+                case "Elon Riverlands":
+                case "Straits of Devastation":
+                case "Vabbi":
+                    byGame.Pof += skillPoints * 10;
+                    break;
+                default:
+                    byGame.Core += skillPoints;
+                    break;
             }
         }
 

Tests/ViewModel/ViewModelMainTest.cs
@@ -36,6 +36,16 @@ namespace Charrmander.ViewModel
             vm.PoIs.Should().BeEmpty();
             vm.Skills.Should().BeEmpty();
             vm.Vistas.Should().BeEmpty();
+
+            vm.SkillPointsTotal.Core.Should().BePositive();
+            vm.SkillPointsTotal.Hot.Should().BePositive();
+            vm.SkillPointsTotal.Pof.Should().BePositive();
+            vm.SkillPointsLocked.Core.Should().Be(vm.SkillPointsTotal.Core);
+            vm.SkillPointsLocked.Hot.Should().Be(vm.SkillPointsTotal.Hot);
+            vm.SkillPointsLocked.Pof.Should().Be(vm.SkillPointsTotal.Pof);
+            vm.SkillPointsUnlocked.Core.Should().Be(0);
+            vm.SkillPointsUnlocked.Hot.Should().Be(0);
+            vm.SkillPointsUnlocked.Pof.Should().Be(0);
         }
 
         [Fact]
@@ -478,6 +488,54 @@ namespace Charrmander.ViewModel
             vm.PoIIcon.Should().Be("PoI");
             vm.SkillIcon.Should().Be("Skill");
             vm.VistaIcon.Should().Be("Vista");
+        }
+
+        [Fact]
+        public void progress_skills_objective_counts_locked_skill_points()
+        {
+            var vm = new ViewModelMain();
+            vm.CommandNewCharacter.Execute(null);
+
+            vm.SelectedAreaReference = vm.AreaReferenceList.First(a => a.Name == "Diessa Plateau");
+            vm.Skills = "1";
+
+            vm.SkillPointsUnlocked.Core.Should().Be(1);
+            vm.SkillPointsLocked.Core.Should().Be(vm.SkillPointsTotal.Core - vm.SkillPointsUnlocked.Core);
+
+            vm.SelectedAreaReference = vm.AreaReferenceList.First(a => a.Name == "Auric Basin");
+            vm.Skills = "2";
+            vm.SkillPointsUnlocked.Hot.Should().Be(20);
+            vm.SkillPointsLocked.Hot.Should().Be(vm.SkillPointsTotal.Hot - vm.SkillPointsUnlocked.Hot);
+
+            vm.SelectedAreaReference = vm.AreaReferenceList.First(a => a.Name == "Crystal Oasis");
+            vm.Skills = "3";
+            vm.SkillPointsUnlocked.Pof.Should().Be(30);
+            vm.SkillPointsLocked.Pof.Should().Be(vm.SkillPointsTotal.Pof - vm.SkillPointsUnlocked.Pof);
+        }
+
+        [Fact]
+        public void change_character_counts_locked_skill_points()
+        {
+            var vm = new ViewModelMain();
+            vm.CommandNewCharacter.Execute(null);
+
+            vm.SelectedAreaReference = vm.AreaReferenceList.First(a => a.Name == "Auric Basin");
+            vm.Skills = "1";
+            vm.SkillPointsUnlocked.Hot.Should().Be(10);
+            vm.SkillPointsLocked.Hot.Should().Be(vm.SkillPointsTotal.Hot - vm.SkillPointsUnlocked.Hot);
+
+            vm.CommandNewCharacter.Execute(null);
+            vm.SelectedCharacter = vm.SortedCharacterList.View.Cast<Character>().Last();
+
+            vm.SkillPointsLocked.Hot.Should().Be(vm.SkillPointsTotal.Hot);
+
+            vm.SelectedAreaReference = vm.AreaReferenceList.First(a => a.Name == "Crystal Oasis");
+            vm.Skills = "2";
+            vm.SkillPointsUnlocked.Pof.Should().Be(20);
+            vm.SkillPointsLocked.Pof.Should().Be(vm.SkillPointsTotal.Pof - vm.SkillPointsUnlocked.Pof);
+
+            vm.SelectedCharacter = vm.SortedCharacterList.View.Cast<Character>().First();
+            vm.SkillPointsLocked.Pof.Should().Be(vm.SkillPointsTotal.Pof);
         }
 
         [Fact]

View/MainWindow.xaml
@@ -718,6 +718,35 @@
                                 Content="Show only required&#10;for world completion"
                                 />
 
+                            <Separator/>
+
+                            <GroupBox Header="Locked Hero Points">
+                                <StackPanel Margin="2">
+
+                                    <StackPanel Orientation="Horizontal">
+                                        <TextBlock Text="Core:" Width="35"/>
+                                        <TextBlock Text="{Binding SkillPointsLocked.Core}" TextAlignment="Right" Width="20"/>
+                                        <TextBlock Text="/" Width="25" TextAlignment="Center"/>
+                                        <TextBlock Text="{Binding SkillPointsTotal.Core}" TextAlignment="Right" Width="20"/>
+                                    </StackPanel>
+
+                                    <StackPanel Orientation="Horizontal">
+                                        <TextBlock Text="HoT:" Width="35"/>
+                                        <TextBlock Text="{Binding SkillPointsLocked.Hot}" TextAlignment="Right" Width="20"/>
+                                        <TextBlock Text="/" Width="25" TextAlignment="Center"/>
+                                        <TextBlock Text="{Binding SkillPointsTotal.Hot}" TextAlignment="Right" Width="20"/>
+                                    </StackPanel>
+
+                                    <StackPanel Orientation="Horizontal">
+                                        <TextBlock Text="PoF:" Width="35"/>
+                                        <TextBlock Text="{Binding SkillPointsLocked.Pof}" TextAlignment="Right" Width="20"/>
+                                        <TextBlock Text="/" Width="25" TextAlignment="Center"/>
+                                        <TextBlock Text="{Binding SkillPointsTotal.Pof}" TextAlignment="Right" Width="20"/>
+                                    </StackPanel>
+
+                                </StackPanel>
+                            </GroupBox>
+
                         </StackPanel>
 
                     </DockPanel>

ViewModel/SkillPointsByGame.cs
@@ -1,0 +1,50 @@
+using Charrmander.Util;
+
+namespace Charrmander.ViewModel
+{
+    internal class SkillPointsByGame : AbstractNotifier
+    {
+        private int _core;
+        public int Core
+        {
+            get => _core;
+
+            set
+            {
+                if (_core != value)
+                {
+                    _core = value;
+                    RaisePropertyChanged(nameof(Core));
+                }
+            }
+        }
+
+        private int _hot;
+        public int Hot
+        {
+            get => _hot;
+            set
+            {
+                if (_hot != value)
+                {
+                    _hot = value;
+                    RaisePropertyChanged(nameof(Hot));
+                }
+            }
+        }
+
+        private int _pof;
+        public int Pof
+        {
+            get => _pof;
+            set
+            {
+                if (_pof != value)
+                {
+                    _pof = value;
+                    RaisePropertyChanged(nameof(Pof));
+                }
+            }
+        }
+    }
+}

commit e172506f5a2d62ef1e0454d61b0840d154b477ba

Fix incorrect area region grouping
Fixes: ffec7a6 (Compute locked skill points on "Areas" tab, 2021-05-26)

VersionHistory.xml
@@ -5,7 +5,7 @@
       <Version>1.24.0.0</Version>
       <DownloadUrl>https://bitbucket.org/jokum/charrmander/downloads/charrmander-1.24.0.zip</DownloadUrl>
       <Notes>
-        <Note></Note>
+        <Note>Fix incorrect region grouping for locked hero point calculation.</Note>
       </Notes>
     </Release>
   </Unreleased>

ViewModel/ViewModelMain.cs
@@ -760,9 +760,9 @@ namespace Charrmander.ViewModel
                     break;
                 case "Crystal Oasis":
                 case "Desert Highlands":
+                case "Domain of Vabbi":
                 case "Elon Riverlands":
-                case "Straits of Devastation":
-                case "Vabbi":
+                case "The Desolation":
                     byGame.Pof += skillPoints * 10;
                     break;
                 default: