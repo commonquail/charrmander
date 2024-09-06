0233492df16bdbbfcce131b3150fea792995894e
Mikkel Kjeldsen <commonquail@gmail.com>
2024-09-06 18:19:10 +0200

    Bump version

--- ViewModel/ViewModelMain.cs
+++ ViewModel/ViewModelMain.cs
@@ -44,7 +44,7 @@ namespace Charrmander.ViewModel
 
         private string _windowTitle = "Charrmander";
 
-        private readonly Version _curVersion = new(1, 42, 0, 0);
+        private readonly Version _curVersion = new(1, 43, 0, 0);
 
         private string _statusBarUpdateCheck = default!;
 
